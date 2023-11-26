1224a982ec70c602a92ff4f68d554e321e667f4a
Judicaeld17e <judicaeldjidonou3@gmail.com>
2023-11-26 10:07:24 +0100

    first commit

---
+++ my_pandas_journey_extract_columns.py
@@ -0,0 +1,4 @@
+#TASK : Create a function which receive a pandas dataframe as parameter and returns columns name.
+import pandas as pd
+def my_pandas_journey_extract_columns(df):
+    return df.columns
